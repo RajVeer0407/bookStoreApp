0a71ff614a0ca6cb375151196a4a097aca5adcfd
RajVeer0407 <er.rajveershaw@gmail.com>
2024-11-21 18:23:04 +0530

    Update 2 !!

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bookRoute from "./route/book.route.js";
+import userRoute from './route/user.route.js'
 import cors from 'cors'
 
 const app = express();
 
 app.use(cors())
+app.use(express.json())
 
 dotenv.config();
 
@@ -26,6 +28,7 @@ try {
 
 //defining routes
 app.use("/book", bookRoute)
+app.use("/user", userRoute)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
