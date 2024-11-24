407da663e7d7851859917534ea6e643c110d0fed
RajVeer0407 <er.rajveershaw@gmail.com>
2024-11-24 11:32:58 +0530

    Final Update  !!

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -16,7 +16,11 @@ export const signup = async (req,res)=>{
             password: hashPassword
         })
         await createdUser.save()
-        res.status(201).json({message: "User created successfully"})
+        res.status(201).json({message: "User created successfully", user:{
+            _id: createdUser._id,
+            fullname: createdUser.fullname,
+            email: createdUser.email,
+        }})
     } catch (error) {
         console.log("Error: " + error.message);
         res.status(500).json({message: "Internal server error"})
@@ -32,9 +36,9 @@ export const login = async (req, res) => {
             return res.status(400).json({message:"Invalid Username or Password"})
         }else{
             res.status(200).json({message:"Login Succesful", user:{
-                _id:user._id,
-                fullname:user.fullname,
-                email:user.email
+                _id: user._id,
+                fullname: user.fullname,
+                email: user.email
             }})
         }
     } catch (error) {
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useLocation, useNavigate} from 'react-router-dom'
 import Login from './Login';
 import { useForm } from 'react-hook-form';
+import axios from "axios"
+import toast from 'react-hot-toast';
 
 function Signup() {
+  const location=useLocation()
+  const navigate = useNavigate()
+  const from = location.state?.from?.pathname || "/"
   const {register,
     handleSubmit,
   formState: {errors},
 } = useForm();
 
-const onSubmit = (data) => console.log(data)
+const onSubmit = async (data) => {
+  const userInfo = {
+    fullname: data.fullname,
+    email: data.email,
+    password: data.password,
+  };
+
+  await axios
+  .post("http://localhost:4001/user/signup", userInfo)
+  .then((res)=>{
+    // console(res.data)
+    if(res.data){
+      toast.success('SignUp Succesfull');
+      navigate(from,{replace:true});
+    }
+    localStorage.setItem("Users", JSON.stringify(res.data.user))
+  })
+  .catch((err)=>{
+    if (err.response) {
+      console.log(err)
+      toast.error("Error: " + err.response.data.message);
+    }
+  })
+}
   return (
     <>
     <div className='flex h-screen items-center justify-center border sha'>
@@ -25,9 +53,9 @@ const onSubmit = (data) => console.log(data)
         <br />
         <input type="text"
         placeholder='Enter your Full Name' className='w-80 py-2 px-3 border rounded-md outline-none' 
-        {...register("name", { required: true })}/>
+        {...register("fullname", { required: true })}/>
         <br />
-              {errors.name && <span className='text-sm text-red-500'>This field is required</span>}
+              {errors.fullname && <span className='text-sm text-red-500'>This field is required</span>}
     </div>
     <div className='mt-4 space-y-2'>
         <span>Email</span>
